88eb1e88ca0cf6f69f69b20714ef12f02c5c38f2
Roman <43014719+RomanDietenmeier@users.noreply.github.com>
2021-05-26 17:09:35 +0200

    Man kann jetzt das Programm mit Parameter test oder clean starten dann wird im Arbeitsverzeichnis mal beispielsweise eine Datei mit dem was zu tun angelsegt anstatt die Datenbank zu bearbetien! (F:\stuff\qt\build-Implementierung-Desktop_Qt_5_15_2_MinGW_64_bit-Debug war bei mir das Verzeichnis)

--- main.cpp
+++ main.cpp
@@ -3,17 +3,20 @@
 #include <QApplication>
 #include<string>
 #include<iostream>
+#include<fstream>
 
 int main(int argc, char *argv[])
-{
+{    
     //auf test oder clean aufruf ueberpruefen
     if(argc>1){
+        std::ofstream ofs("out.txt",std::ofstream::out);
         std::string arg1=argv[1];
-        if(arg1.compare("clean")){
-            std::cout<<"alle verwendeten Datenbanktabellen in den „Auslieferungszustand“ zurückversetzen"<<std::endl;
-        }else if(arg1.compare("test")){
-            std::cout<<"Datenbanktabellen mit Testdaten"<<std::endl;
+        if(arg1.compare("clean")==0){
+            ofs<<"alle verwendeten Datenbanktabellen in den „Auslieferungszustand“ zurückversetzen"<<std::endl;
+        }else if(arg1.compare("test")==0){
+           ofs<<"Datenbanktabellen mit Testdaten"<<std::endl;
         }
+        ofs.close();
     }
     QApplication a(argc, argv);
     MainWindow w;
